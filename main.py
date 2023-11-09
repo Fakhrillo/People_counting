1eb98b9791a0ec499e5f0aa4f2e37922cbcf00e7
Fakhrillo <dombit1999@gmail.com>
2023-11-09 18:14:18 +0500

    colors

--- main.py
+++ main.py
@@ -109,6 +109,9 @@ with dai.Device(pipeline, device) as device:
             startTime = current_time
 
         color = (255, 0, 0)
+        text_color = (0, 0, 255)
+        rectangle = (111, 147, 26)
+
         frame = imgFrame.getCvFrame()
         trackletsData = track.tracklets
         for t in trackletsData:
@@ -124,17 +127,17 @@ with dai.Device(pipeline, device) as device:
                 except:
                     label = t.label
                 # if t.status.name == 'TRACKED':
-                cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-                cv2.putText(frame, f"ID: {[t.id]}", (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-                cv2.putText(frame, t.status.name, (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), color, cv2.FONT_HERSHEY_SIMPLEX)
+                cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, text_color)
+                cv2.putText(frame, f"ID: {[t.id]}", (x1 + 10, y1 + 45), cv2.FONT_HERSHEY_TRIPLEX, 0.5, text_color)
+                cv2.putText(frame, t.status.name, (x1 + 10, y1 + 70), cv2.FONT_HERSHEY_TRIPLEX, 0.5, text_color)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), rectangle, cv2.FONT_HERSHEY_SIMPLEX)
 
             # if t.status.name == 'REMOVED':
             #     print(f'ID: {t.id} X1: {x1}, Y1: {y1}, X2: {x2}, Y2: {y2} STATUS: {t.status.name} !!!!!!!')
             # else:
             #     print(f'ID: {t.id} X1: {x1}, Y1: {y1}, X2: {x2}, Y2: {y2} STATUS: {t.status.name}')
 
-        cv2.putText(frame, "FPS: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
+        cv2.putText(frame, "FPS: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.6, text_color)
 
         cv2.imshow("tracker", frame)
 
